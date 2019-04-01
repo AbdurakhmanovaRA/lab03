435d5066edaaf0f01a59f52a41c224dfa755fa5c
u211-04 <u211-04@public.mpei.local>
2019-04-01 10:43:22 +0300

    code: добавление функций make_histogram и show_histogram_txt

--- main.cpp
+++ main.cpp
@@ -24,33 +24,22 @@ void find_minmax(vector<double> numbers, double& min, double& max) {
     }
 }
 
-int main() {
-    // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-
-    cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-    // Обработка данных
-    double min, max;
-find_minmax(numbers, min, max);
-
-    vector<size_t> bins(bin_count);
+vector<size_t> make_histogram(vector<double> numbers,size_t count){
+    double min = numbers[0];
+    double max = numbers[0];
+    find_minmax(numbers, min, max);
+    vector<size_t> bins(count);
     for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
+        size_t bin = (size_t)((number - min) / (max - min) * count);
+        if (bin == count) {
             bin--;
         }
         bins[bin]++;
     }
+    return bins;
+}
 
-    // Вывод данных
+void show_histogram_txt(vector<size_t> bins){
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -61,8 +50,7 @@ find_minmax(numbers, min, max);
         }
     }
     const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins) {
+     for (size_t bin : bins) {
         if (bin < 100) {
             cout << ' ';
         }
@@ -82,6 +70,28 @@ find_minmax(numbers, min, max);
         }
         cout << '\n';
     }
+}
+
+int main() {
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+    // Обработка данных
+
+    vector<size_t> bins = make_histogram(numbers, bin_count);
+
+    // Вывод данных
+
+    show_histogram(bins);
 
     return 0;
 }
